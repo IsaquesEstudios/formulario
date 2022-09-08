8367314b44711818f6a25f6106ea53daaa2cb2fb
Matteus Isaque <matteus.isaque28@gmail.com>
2022-09-08 11:20:15 -0300

    algo

--- src/app.js
+++ src/app.js
@@ -72,7 +72,7 @@ app.post("/ticonnected", (req, res) => {
 
     const { emailTo, title, domain, name, email, phone, state, city, branches, companyName, message } = req.body
 
-    ejs.renderFile(__dirname + "/template/email-number.ejs", {
+    ejs.renderFile(__dirname + "/template/ticonnected.ejs", {
         domain: domain,
         name: name,
         email: email,
